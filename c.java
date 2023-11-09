5df5787fd4aecdc48bd79005ad5c123bfa50f3de
Sergio Holguera <edisonseryo@gmail.com>
2023-11-09 21:27:56 +0100

    add setMethod clas Persona

--- c.java
+++ c.java
@@ -1,3 +1,11 @@
 public class Persona{
+    String nombre;
     
+    public Persona(String nombre){
+        this.nombre = nombre
+    }
+
+    public void setNombre(Stirng nombre){
+        this.nombre = nombre;
+    }
 }
